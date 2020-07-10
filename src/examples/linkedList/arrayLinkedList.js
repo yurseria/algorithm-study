e85446872d0950fde9dbe9ff15d4c9911c4bcea6
yurseria <yurseria@gmail.com>
2020-07-10 23:01:29 +0900

    feat: Divide a class into object parts and function parts.

--- src/examples/linkedList/arrayLinkedList.js
+++ src/examples/linkedList/arrayLinkedList.js
@@ -1,16 +1,45 @@
 const inquirer = require("inquirer");
+let arr = [];
 
 class ArrayLinkedList {
   constructor() {
-    this.goBackMessage = "Go Back.";
-    this.arr = [];
     this.count = 0;
   }
 
-  async show() {
+  show() {
     for (let i = 0; i < this.count; i++) {
-      console.log(`${i}: ${this.arr[i]}`);
+      console.log(`${i}: ${arr[i]}`);
     }
+  }
+
+  addBack(data) {
+    arr[this.count] = data;
+    this.count++;
+  }
+
+  addFirst(data) {
+    for (let i = this.count; i >= 1; i--) {
+      arr[i] = arr[i - 1];
+    }
+    arr[0] = data;
+    this.count++;
+  }
+
+  removeAt(index) {
+    for (let i = Number(index); i < this.count - 1; i++) {
+      arr[i] = arr[i + 1];
+    }
+    this.count--;
+  }
+}
+class ArrayLinkedListFunction {
+  constructor() {
+    this.goBackMessage = "Go Back.";
+    this.linkedList = new ArrayLinkedList();
+  }
+
+  async show() {
+    this.linkedList.show();
 
     await this.execute();
   }
@@ -23,8 +52,7 @@ class ArrayLinkedList {
         message: "Enter data to add after the array.",
       })
       .then(async (answer) => {
-        this.arr[this.count] = answer.data;
-        this.count++;
+        this.linkedList.addBack(answer.data);
 
         await this.execute();
       });
@@ -38,18 +66,14 @@ class ArrayLinkedList {
         message: "Enter data to add before the array.",
       })
       .then(async (answer) => {
-        for (let i = this.count; i >= 1; i--) {
-          this.arr[i] = this.arr[i - 1];
-        }
-        this.arr[0] = answer.data;
-        this.count++;
+        this.linkedList.addFirst(answer.data);
 
         await this.execute();
       });
   }
 
   async removeAt() {
-    if (this.count <= 0) {
+    if (this.linkedList.count <= 0) {
       console.error("No element!");
       await this.execute();
       return;
@@ -62,19 +86,13 @@ class ArrayLinkedList {
         message: "Enter the index to delete from the array.",
       })
       .then(async (answer) => {
-        if (
-          this.arr[answer.index] === null ||
-          this.arr[answer.index] === undefined
-        ) {
+        if (arr[answer.index] === null || arr[answer.index] === undefined) {
           console.error("Wrong index!");
           await this.execute();
           return;
         }
 
-        for (let i = Number(answer.index); i < this.count - 1; i++) {
-          this.arr[i] = this.arr[i + 1];
-        }
-        this.count--;
+        this.linkedList.removeAt(answer.index);
 
         await this.execute();
       });
@@ -97,6 +115,7 @@ class ArrayLinkedList {
         })
         .then(async (answers) => {
           if (answers.function === this.goBackMessage) {
+            arr = [];
             resolve();
             return;
           }
@@ -107,4 +126,4 @@ class ArrayLinkedList {
   }
 }
 
-module.exports = ArrayLinkedList;
+module.exports = ArrayLinkedListFunction;
